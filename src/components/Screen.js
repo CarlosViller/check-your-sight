26944b95ee5c9b7872ff5a13a6a2409b53efbcfd
Deltaaaaq <carlos.viller98@gmail.com>
2022-07-31 10:48:32 -0300

    move 1 level

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,33 +1,89 @@
-import { useRef, useState } from "react"
-import MovingColor from "./MovingColor";
+import { useRef, useState, useEffect } from "react"
 import newColors from "../colors";
+import { ENDED, STARTED, POINT, FAILED } from "../constants";
 
-export default function Screen({ addPoint, lose, state, spread = 10 }) {
+const BASE_SIZE = 20
+
+function useOutsideHandler(ref, screenRef, status, handler) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        status === STARTED &&
+        ref.current &&
+        !ref.current.contains(event.target) &&
+        screenRef.current.contains(event.target)) {
+        handler()
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, screenRef]);
+}
+
+function useCoords(screenRef) {
+  const [state, setState] = useState({
+    x: randomMove(screenRef.current?.offsetWidth),
+    y: randomMove(screenRef.current?.offsetHeight)
+  })
+
+  function changeCoords() {
+    setState({
+      x: randomMove(screenRef.current?.offsetWidth),
+      y: randomMove(screenRef.current?.offsetHeight)
+    })
+  }
+
+  return [state, changeCoords]
+}
+
+export default function Screen({ dispatch, state, spread = 10 }) {
 
   // Format: { bkg: rgbColor <string>, winner: rgbColor <string> }
   const [colors, setColors] = useState(newColors(spread))
   const screenRef = useRef(null)
+
+  const blockRef = useRef(null);
+  useOutsideHandler(blockRef, screenRef, state.status, () => dispatch({type: FAILED}));
+
+  const [coords, changeCoords] = useCoords(screenRef)
 
   return (
     <div
       ref={screenRef}
       css={{
         position: "relative",
-        width: "50%",
-        height: "calc(100% - 125px)",
+        width: "60%",
+        height: "calc(100% - 145px)",
         background: `${colors.bkg}`,
-        margin: "0 auto",
+        margin: "10px auto",
       }}
     >
-      <MovingColor
-        winnerColor={colors.winner}
-        status={state.status}
-        addPoint={addPoint}
-        lose={lose}
-        setColors={setColors}
-        spread={spread}
-        screenRef={screenRef}
-      />
+      <button
+        ref={blockRef}
+        css={{
+          width: BASE_SIZE,
+          height: BASE_SIZE,
+          padding: 0,
+          background: `${colors.winner}`,
+          border: state.status === ENDED ? "1px solid white" : "unset",
+          position: "absolute",
+          left: coords.x,
+          top: coords.y
+
+        }}
+        onClick={() => {
+          if (state.status !== ENDED) {
+            dispatch({ type: POINT })
+            setColors(newColors(spread))
+            changeCoords()
+          }
+        }}
+      >
+      </button>
     </div>
   )
 }
+
+const randomMove = (max) => max ? `${Math.floor(Math.random() * (max - BASE_SIZE))}px` : 0
